c78285ccc41c42b973d82dc363d76a735009e40e
Doodleboo <174210143+Doodleboo@users.noreply.github.com>
2024-08-13 19:52:00 +0200

    PIF version

--- Quick-Resize.lua
+++ Quick-Resize.lua
@@ -2,26 +2,16 @@ function init(plugin)
   function QuickResize(scaleFactor)
     app.command.SpriteSize { ui=false, scale=scaleFactor, method="nearest" }
   end
+  
+  function QuickResizeAbs(size)
+	app.command.SpriteSize { ui=false, width=96, height=96, method="nearest" }
+  end
 
   plugin:newCommand{
-    id="QuickResize_Quarter",
-    title="Quick Resize 25%",
+    id="QuickResize_96",
+    title="Quick Resize 96px",
     group="sprite_size",
-    onclick=function() QuickResize(0.25) end
-  }
-
-  plugin:newCommand{
-    id="QuickResize_Half",
-    title="Quick Resize 50%",
-    group="sprite_size",
-    onclick=function() QuickResize(0.5) end
-  }
-
-  plugin:newCommand{
-    id="QuickResize_Double",
-    title="Quick Resize 200%",
-    group="sprite_size",
-    onclick=function() QuickResize(2) end
+    onclick=function() QuickResizeAbs(96) end
   }
 
   plugin:newCommand{
@@ -29,13 +19,6 @@ function init(plugin)
     title="Quick Resize 300%",
     group="sprite_size",
     onclick=function() QuickResize(3) end
-  }
-
-  plugin:newCommand{
-    id="QuickResize_Quadruple",
-    title="Quick Resize 400%",
-    group="sprite_size",
-    onclick=function() QuickResize(4) end
   }
 
 end
